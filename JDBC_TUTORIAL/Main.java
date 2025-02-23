d99a2272b5a76c345c4c7c78b9e1fb8f00679880
Prashant Singh <113935621+Prash-is-alive@users.noreply.github.com>
2025-02-23 12:24:02 +0530

    ignoring .class files from now

--- JDBC_TUTORIAL/Main.java
+++ JDBC_TUTORIAL/Main.java
@@ -1,13 +1,20 @@
 import java.sql.*;
 
-class Main{
-    public static void main(String[] args) throws Exception{
+class Main {
+    public static void main(String[] args) throws Exception {
 
-        String url = "";
-        String uname = "";
-        String pass = "";
-
-        Class.forName("com.mysql.jsbc.Driver");
+        String url = "jdbc:mysql:localhost:3306/traildb";
+        String uname = "root";
+        String pass = "Pass@123";
+        String query = "select * from students";
         Connection con = DriverManager.getConnection(url, uname, pass);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next())
+            System.out.println(rs.getInt("roll") + " " + rs.getString("name"));
+
+        con.close();
+        st.close();
+        rs.close();
     }
 }
